2f3a65c0d00bc2e806581dfc4fac9f87e1d9b4d5
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-10-02 12:49:02 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,7 @@ def pregunta_01():
     40
 
     """
-    return
+    return tbl0.shape[0]
 
 
 def pregunta_02():
@@ -33,7 +33,7 @@ def pregunta_02():
     4
 
     """
-    return
+    return tbl0.shape[1]
 
 
 def pregunta_03():
@@ -50,7 +50,8 @@ def pregunta_03():
     Name: _c1, dtype: int64
 
     """
-    return
+    xx=tbl0.groupby("_c1").count()
+    return xx["_c2"]
 
 
 def pregunta_04():
@@ -65,7 +66,8 @@ def pregunta_04():
     E    4.785714
     Name: _c2, dtype: float64
     """
-    return
+    xx=tbl0.groupby("_c1")["_c2"].mean()
+    return xx
 
 
 def pregunta_05():
@@ -82,7 +84,7 @@ def pregunta_05():
     E    9
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].max()
 
 
 def pregunta_06():
@@ -94,7 +96,7 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
-    return
+    return tbl1.sort_values("_c4")["_c4"].str.upper().unique()
 
 
 def pregunta_07():
@@ -110,7 +112,7 @@ def pregunta_07():
     E    67
     Name: _c2, dtype: int64
     """
-    return
+    return tbl0.groupby("_c1")["_c2"].sum()
 
 
 def pregunta_08():
@@ -128,7 +130,9 @@ def pregunta_08():
     39   39   E    5  1998-01-26    44
 
     """
-    return
+    xx=tbl0.assign(suma=tbl0._c0 + tbl0._c2)
+    return xx
+
 
 
 def pregunta_09():
@@ -146,7 +150,11 @@ def pregunta_09():
     39   39   E    5  1998-01-26  1998
 
     """
-    return
+    tbl0["year"]=pd.NA
+    for i in range (len(tbl0)):
+        tbl0["year"][i]=tbl0["_c3"][i].split("-")[0]
+        
+    return tbl0
 
 
 def pregunta_10():
@@ -164,7 +172,7 @@ def pregunta_10():
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
     return
-
+print(pregunta_10())
 
 def pregunta_11():
     """
